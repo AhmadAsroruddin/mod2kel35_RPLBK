1aa7ac5cbfc43d198c0cc3d722ab45891d94402e
AnggaRafi <85323173+AnggaRafi@users.noreply.github.com>
2023-09-26 21:38:26 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import './App.css';
 import ReactJSX, {ReactNoJSX} from './components/ReactJsx'; 
 import CompAndProps from './components/compAndProps';
+import Button from './feature/Button';
+import TextInput from './feature/TextInput';
 
 function App() {
+  const [text, setText] = useState("");
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      setText(event.target[0].value);
+    };
+  
   return (
     <div className="App">
       <header className="App-header">
         <ReactJSX />
         <ReactNoJSX />
-<div className="Components"> <CompAndProps bgColor="#66ccff"/>
-</div>
+        <div className="Components"> <CompAndProps bgColor="#66ccff"/>
+        <form onSubmit={handleSubmit}>
+          <TextInput border="red" placeholder="Ketik kata atau tulisan"/>
+          <Button type="submit"background="blue">Kirim</Button>
+        </form>
+          <p >Kata atau tulisan yang anda ketik adalah: {text}</p>
+        </div>
       </header>
     </div>
 ); 
